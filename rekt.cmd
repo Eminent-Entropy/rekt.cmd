c16a406b2add07ba228daea37bd973ac3538e8b5
Aiden G <orca950@gmail.com>
2017-06-09 13:13:28 -0500

    removed needed code, and looks more like cmd

--- rekt.cmd
+++ rekt.cmd
@@ -3,10 +3,7 @@ REM get around cmd block
 
 color D
 title REKT
-SET /a "x = 0"
 :while1
-	if %x% leq 5 (
-		%Input%
-		SET /P Input=%cd%\
-		goto :while1
-	)
+	%Input%
+	SET /P Input="%cd%>"
+	goto :while1
